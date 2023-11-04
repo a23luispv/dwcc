bc543c596fb463b5b4dff63e4c65caf67b7ac5a4
a23luispv <a23luispv@iessanclemente.net>
2023-11-04 12:41:57 +0100

    resolucion fibonacci

---
+++ ej02/fibonacci.js
@@ -0,0 +1,17 @@
+let a = 0;
+let b = 1;
+let temporal;
+
+console.log('primeros 10:')
+for(i=0; i <=9; i++){
+    //console.log(`vuelta ${i}`)
+    console.log(b)
+   
+
+    temporal = a;
+    a=b;
+    b=temporal+b;
+
+    
+
+}
