b162eceaf1f774b515862447ddfef1ecd756b456
a23luispv <a23luispv@iessanclemente.net>
2023-12-02 13:18:28 +0100

    ud04_ej01_mostrartodoarray

--- ud04/ej01.js
+++ ud04/ej01.js
@@ -1,4 +1,7 @@
 let paises = ["España", "Portugal", "Francia", "Irlanda"];
 
 //Amosa o número de elementos do Array
-console.log("Nº paises array: "+paises.length);
+console.log("Nº paises array: " + paises.length);
+
+//Amosa todos os países.
+console.log("Todos os paises: " + paises);
